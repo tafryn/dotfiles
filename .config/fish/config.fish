d58a127ce4f18c315631785d52a69d3eec5dafa8
tafryn <tafryn@gmail.com>
2015-12-03 16:57:51 -0800

    Add basic config.fish.

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+# PATH is handled specially by fish so the elements are space separated
+# instead of colon separated.
+set -gx PATH $PATH /sbin /usr/sbin $HOME/bin $HOME/.cabal/bin
+
+set -gx LD_LIBRARY_PATH /usr/local/lib64:/usr/local/lib
