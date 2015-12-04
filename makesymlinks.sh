074a1c2022657cf10d8beb65e03677315bcb4d94
tafryn <tafryn@gmail.com>
2015-12-03 16:58:48 -0800

    Include config.fish in symlink script.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -16,7 +16,8 @@ change_counter=0                  # count of changes made
 # list of files/folders to symlink in homedir
 files="gdbinit irssi mailcap pentadactylrc screenrc tmux.conf tmux\
        vimpagerrc vimrc vim Xdefaults xinitrc xmobarrc xmonad zshenv zshrc zsh\
-       config/fish/functions/zish_greeting.fish config/fish/functions/zish_prompt.fish"
+       config/fish/functions/zish_greeting.fish config/fish/functions/zish_prompt.fish\
+       config/fish/config.fish"
 
 # }}} #
 
