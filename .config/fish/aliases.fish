1da8379fadb662068dda876ffc26e22d0e1cc3c1
tafryn <tafryn@gmail.com>
2015-12-07 14:47:39 -0800

    Add ls function tweak, and environment based aliases.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,3 +1,23 @@
+if command --search "vimpager" >/dev/null
+    set -x PAGER "vimpager"
+    set -x MANPAGER "vimpager"
+    alias less "vimpager"
+else if command --search "most" >/dev/null
+    set -x MANPAGER 'most'
+    alias less "most"
+else
+    alias less "less -r"
+end
+
+if command --search "systemctl" >/dev/null
+    alias poweroff "sudo systemctl poweroff"
+    alias restart "sudo systemctl restart"
+end
+
+if command --search "vimcat" >/dev/null
+    alias cat "vimcat"
+end
+
 alias cls "clear; ls"
 alias grep "grep --color=auto --binary-files=without-match --exclude-dir=.svn -n"
 alias socks "ssh -f -N -D 8080"
