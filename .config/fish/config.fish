0f31201199151bf147427c7ffd856e36cdc53b2a
tafryn <tafryn@gmail.com>
2016-01-22 09:13:06 -0800

    Set additional environment variables in fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,15 @@
 . ~/.config/fish/aliases.fish
 
 # Environment Variables
+set -gx LC_COLLATE "C"
 set -x EDITOR "vim"
 set -gx MANPATH (manpath)
 
 # PATH is handled specially by fish so the elements are space separated
 # instead of colon separated.
 set -gx PATH $PATH /sbin /usr/sbin $HOME/bin $HOME/.cabal/bin
+
+set -U fish_user_paths /usr/local/bin /usr/bin
 
 set -gx LD_LIBRARY_PATH /usr/local/lib64:/usr/local/lib
 
