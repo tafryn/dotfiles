b52a6ab4f0f2d65cb8ebd02702e9d1cc5a745b92
tafryn <tafryn@gmail.com>
2017-01-05 09:29:13 -0800

    Update fish path.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ set -gx MANPATH (manpath)
 
 # PATH is handled specially by fish so the elements are space separated
 # instead of colon separated.
-set -gx PATH $PATH /sbin /usr/sbin $HOME/bin $HOME/.cabal/bin
+set -gx PATH $PATH /sbin /usr/sbin $HOME/bin $HOME/sfw/bin $HOME/.cabal/bin
 
 set -U fish_user_paths /usr/local/bin /usr/bin
 
