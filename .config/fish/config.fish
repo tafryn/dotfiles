1fc43dab12e0e4e119cdc6766524c4477d780c65
tafryn <tafryn@gmail.com>
2019-01-16 16:19:19 -0800

    Set BROWSER environment variable.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ set -gx GCC_COLORS "on"
 set -gx TZ ":/etc/localtime"
 set -x EDITOR "vim"
 set -x TERMINAL "alacritty"
+set -x BROWSER "vivaldi"
 
 # PATH is handled specially by fish so the elements are space separated
 # instead of colon separated.
