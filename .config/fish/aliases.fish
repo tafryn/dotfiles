17bf54211a46f1daea50eda1c634afeb2d8a1f67
tafryn <tafryn@gmail.com>
2020-12-08 18:50:34 -0800

    Use lazygit for dotfiles alias if available

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -36,7 +36,11 @@ alias elog "colortail --config=/etc/colortail/conf.messages -f /var/log/everythi
 alias tmux "tmux -2"
 alias build "mkdir -p build_dir; and pushd build_dir; and cmake ../; and make -j8; and popd"
 alias tags "ctags -R -f .ctags --file-scope=yes --sort=yes --fields=+iaS --extras=+q"
-alias dotfiles "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+if command --search "lazygit" >/dev/null
+    alias dotfiles "lazygit -g .dotfiles/ -w ~/"
+else
+    alias dotfiles "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+end
 
 #alias git "git --no-pager"
 alias gd "git diff --color -w"
