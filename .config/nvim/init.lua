d3b2af5080252dce7be294f0c62629fb43436472
tafryn <tafryn@gmail.com>
2021-07-06 11:11:59 -0700

    Sync with LunarVim 0.4.0

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,32 @@
-require('default-config')
-vim.cmd('luafile ' .. CONFIG_PATH .. '/lv-config.lua')
-require('settings')
-require('plugins')
-require('lv-autocommands')
-require('mappings')
+require "default-config"
+-- require "keymappings"
+vim.cmd("luafile " .. CONFIG_PATH .. "/lv-config.lua")
 vim.cmd('source '..CONFIG_PATH..'/vimscript/jellybeans/init.vim') -- Must be included before colorscheme is set
 require('highlights') -- Must be included before colorscheme is set
-vim.g.colors_name = O.colorscheme
-vim.g.syntax = true
-require('lv-utils')
-require('lv-galaxyline')
--- require('lv-telescope')
-require('lv-treesitter')
-require('lv-which-key')
+require "settings"
+require "plugins"
+require('lv-autocommands')
+require('mappings') -- TODO: Reconcile LunarVim mappings with personal mappings
+require "lv-utils"
+require "lv-galaxyline"
+require "lv-treesitter"
+require "lv-which-key"
 -- TODO is there a way to do this without vimscript
-vim.cmd('source '..CONFIG_PATH..'/vimscript/functions.vim')
-require('lsp')
-if O.lang.emmet.active then require('lsp.emmet-ls') end
+vim.cmd('source '..CONFIG_PATH..'/vimscript/functions.vim') -- TODO: review functions.vim for redundancies
+require "lsp"
+if O.lang.emmet.active then
+  require "lsp.emmet-ls"
+end
+
+-- autoformat
+if O.format_on_save then
+  require("lv-utils").define_augroups {
+    autoformat = {
+      {
+        "BufWritePre",
+        "*",
+        [[try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry]],
+      },
+    },
+  }
+end
