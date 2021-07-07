0a6a7aadb3f80080b162851b6486312a10288b01
tafryn <tafryn@gmail.com>
2021-07-07 10:28:39 -0700

    Add stylua settings and format lv-config

--- .config/nvim/lv-config.lua
+++ .config/nvim/lv-config.lua
@@ -5,7 +5,8 @@ Linters should be
 filled in as strings with either
 a global executable or a path to
 an executable
-]] -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 -- general
 O.format_on_save = false
 O.auto_complete = true
@@ -25,7 +26,7 @@ O.cursorline = false
 -- TODO User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 O.plugin.dashboard.active = true
-O.plugin.colorizer.active = false
+O.plugin.colorizer.active = true
 O.plugin.ts_playground.active = false
 O.plugin.ts_fold.active = true
 O.plugin.indent_line.active = false
@@ -33,26 +34,26 @@ O.plugin.zen.active = false
 
 -- dashboard
 O.dashboard.custom_header = {
-	[=[⠀⠀⠀⠀⠀⡠⢂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
-	[=[⠀⠀⠀⡠⢊⠔⡐⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⡅⢕⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
-	[=[⠀⡠⢌⢂⢑⠌⠄⠅⡡⢂⠀⠀⠀⠀⠀⠀⠀⠨⡨⠢⡡⡑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
-	[=[⢊⢌⠢⡑⢄⠅⠅⠅⡂⢂⢂⠀⠀⠀⠀⠀⠀⠨⡘⢌⠢⡊⡢⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
-	[=[⡑⢔⢑⢌⢢⢡⠡⡁⡂⠅⡂⠢⡀⠀⠀⠀⠀⠨⡪⡨⢪⠨⡢⡁⠀⠀⠀⠀⠀⠀⢰⡆⢀⡤⠖⠒⠒⠶⣤⠀⠀⠀⠀⢀⡤⠖⠒⠒⠒⢦⣄⠀⠀⠀⠀⣠⡴⠖⠒⠲⠶⣤⡀⠀⢹⣿⣿⡆⠀⠀⠀⠀⠀⣾⣿⡿⠀⣿⣿⣿⠀⠀⣿⣿⣷⣴⣾⣿⣿⣿⣦⣀⣴⣾⣿⣿⣿⣷⣦⠀]=],
-	[=[⡊⡢⡑⠔⢅⢆⠑⡐⢄⠑⢄⢑⠐⠄⠀⠀⠀⢨⠢⡊⡆⡣⡊⡂⠀⠀⠀⠀⠀⠀⢸⡷⠉⠀⠀⠀⠀⠀⠸⣧⠀⠀⣰⠏⠀⠀⠀⠀⠀⠀⠘⣆⠀⢠⡾⠁⠀⠀⠀⠀⠀⠈⢻⣆⠀⢻⣿⣿⡀⠀⠀⠀⣼⣿⣿⠁⠀⣿⣿⣿⠀⠀⣿⣿⣿⠋⠁⠀⠈⢻⣿⣿⡟⠁⠀⠀⠙⣿⣿⡇]=],
-	[=[⡊⡢⡊⢎⢢⢑⠀⠐⠄⢕⠐⢄⠅⢅⢅⠀⠀⠰⡑⡕⢌⢆⠣⠅⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⣿⣤⣤⣤⣤⣤⣤⣤⣤⣿⠀⣼⡇⠀⠀⠀⠀⠀⠀⠀⠈⣿⡀⠈⢿⣿⣷⠀⠀⣰⣿⣿⠃⠀⠀⣿⣿⣿⠀⠀⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⣿⣿⡇]=],
-	[=[⡪⢢⠱⡡⢣⢑⠀⠀⠈⠄⢕⢐⢑⢐⠄⢕⢀⢘⢌⢎⢪⠢⡋⡎⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⠘⣿⣿⣇⢠⣿⣿⠏⠀⠀⠀⣿⣿⣿⠀⠀⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⣿⣿⡇]=],
-	[=[⡪⡢⢣⢱⢡⠣ ⠀⠀⠀⠑⡐⢔⢐⢑⢐⠔⢌⢆⢇⢕⠕⡕⡂⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⢻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣧⠀⠀⠀⠀⠀⠀⠀⣰⡟⠀⠀⠀⠘⣿⣿⣼⣿⡟⠀⠀⠀⠀⣿⣿⣿⠀⠀⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⣿⣿⡇]=],
-	[=[⡪⡸⡸⡰⡑⢭⠀⠀⠀⠀⠀⠈⡂⡢⡑⡐⡑⢔⠱⡸⡰⡱⡱⡁⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠙⠷⣤⣀⣀⣀⣤⠶⠃⠀⠀⠘⠳⣤⣀⣀⣀⣠⠶⠋⠀⠀⠀⠀⠀⠹⣿⣿⡟⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⣿⣿⡇]=],
-	[=[⡪⡪⡢⡣⡣⡣ ⠀⠀⠀⠀⠀⠀⠢⠢⡑⢌⠢⡣⡑⢕⢕⢕⠅⠀⠀⠀⠀⠀⠀                                                                      ]=],
-	[=[⠈⠊⡎⡎⡎⡎ ⠀⠀⠀⠀⠀⠀⠀⠑⢌⠢⢱⢘⢌⢆⢣⠃⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
-	[=[⠀⠀⠈⠪⡪⡪⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡪⡘⡔⡅⠃⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
-	[=[⠀⠀⠀⠀⠈⠪ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠪⠂⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
+  [=[⠀⠀⠀⠀⠀⡠⢂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
+  [=[⠀⠀⠀⡠⢊⠔⡐⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⡅⢕⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
+  [=[⠀⡠⢌⢂⢑⠌⠄⠅⡡⢂⠀⠀⠀⠀⠀⠀⠀⠨⡨⠢⡡⡑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
+  [=[⢊⢌⠢⡑⢄⠅⠅⠅⡂⢂⢂⠀⠀⠀⠀⠀⠀⠨⡘⢌⠢⡊⡢⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
+  [=[⡑⢔⢑⢌⢢⢡⠡⡁⡂⠅⡂⠢⡀⠀⠀⠀⠀⠨⡪⡨⢪⠨⡢⡁⠀⠀⠀⠀⠀⠀⢰⡆⢀⡤⠖⠒⠒⠶⣤⠀⠀⠀⠀⢀⡤⠖⠒⠒⠒⢦⣄⠀⠀⠀⠀⣠⡴⠖⠒⠲⠶⣤⡀⠀⢹⣿⣿⡆⠀⠀⠀⠀⠀⣾⣿⡿⠀⣿⣿⣿⠀⠀⣿⣿⣷⣴⣾⣿⣿⣿⣦⣀⣴⣾⣿⣿⣿⣷⣦⠀]=],
+  [=[⡊⡢⡑⠔⢅⢆⠑⡐⢄⠑⢄⢑⠐⠄⠀⠀⠀⢨⠢⡊⡆⡣⡊⡂⠀⠀⠀⠀⠀⠀⢸⡷⠉⠀⠀⠀⠀⠀⠸⣧⠀⠀⣰⠏⠀⠀⠀⠀⠀⠀⠘⣆⠀⢠⡾⠁⠀⠀⠀⠀⠀⠈⢻⣆⠀⢻⣿⣿⡀⠀⠀⠀⣼⣿⣿⠁⠀⣿⣿⣿⠀⠀⣿⣿⣿⠋⠁⠀⠈⢻⣿⣿⡟⠁⠀⠀⠙⣿⣿⡇]=],
+  [=[⡊⡢⡊⢎⢢⢑⠀⠐⠄⢕⠐⢄⠅⢅⢅⠀⠀⠰⡑⡕⢌⢆⠣⠅⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⣿⣤⣤⣤⣤⣤⣤⣤⣤⣿⠀⣼⡇⠀⠀⠀⠀⠀⠀⠀⠈⣿⡀⠈⢿⣿⣷⠀⠀⣰⣿⣿⠃⠀⠀⣿⣿⣿⠀⠀⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⣿⣿⡇]=],
+  [=[⡪⢢⠱⡡⢣⢑⠀⠀⠈⠄⢕⢐⢑⢐⠄⢕⢀⢘⢌⢎⢪⠢⡋⡎⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⠘⣿⣿⣇⢠⣿⣿⠏⠀⠀⠀⣿⣿⣿⠀⠀⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⣿⣿⡇]=],
+  [=[⡪⡢⢣⢱⢡⠣ ⠀⠀⠀⠑⡐⢔⢐⢑⢐⠔⢌⢆⢇⢕⠕⡕⡂⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⢻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣧⠀⠀⠀⠀⠀⠀⠀⣰⡟⠀⠀⠀⠘⣿⣿⣼⣿⡟⠀⠀⠀⠀⣿⣿⣿⠀⠀⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⣿⣿⡇]=],
+  [=[⡪⡸⡸⡰⡑⢭⠀⠀⠀⠀⠀⠈⡂⡢⡑⡐⡑⢔⠱⡸⡰⡱⡱⡁⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠙⠷⣤⣀⣀⣀⣤⠶⠃⠀⠀⠘⠳⣤⣀⣀⣀⣠⠶⠋⠀⠀⠀⠀⠀⠹⣿⣿⡟⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⣿⣿⡇]=],
+  [=[⡪⡪⡢⡣⡣⡣ ⠀⠀⠀⠀⠀⠀⠢⠢⡑⢌⠢⡣⡑⢕⢕⢕⠅⠀⠀⠀⠀⠀⠀                                                                      ]=],
+  [=[⠈⠊⡎⡎⡎⡎ ⠀⠀⠀⠀⠀⠀⠀⠑⢌⠢⢱⢘⢌⢆⢣⠃⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
+  [=[⠀⠀⠈⠪⡪⡪⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡪⡘⡔⡅⠃⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
+  [=[⠀⠀⠀⠀⠈⠪ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠪⠂⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]=],
 }
 -- O.dashboard.footer = {""}
 
 -- if you don't want all the parsers change this to a table of the ones you want
 O.treesitter.ensure_installed = "maintained"
-O.treesitter.ignore_install = {"haskell"}
+O.treesitter.ignore_install = { "haskell" }
 O.treesitter.highlight.enabled = true
 
 O.lang.clang.diagnostics.virtual_text = true
@@ -77,111 +78,124 @@ O.lang.tsserver.linter = nil
 O.lang.php.environment.php_version = "7.4"
 O.lang.php.diagnostics.signs = true
 O.lang.php.diagnostics.underline = true
-O.lang.php.filetypes = {"php", "phtml"}
+O.lang.php.filetypes = { "php", "phtml" }
 O.lang.php.format = {
   format = {
-    default = "psr12"
-  }
+    default = "psr12",
+  },
 }
 
+vim.g["neoformat_enabled_lua"] = { "stylua" }
+vim.g["neoformat_lua_stylua"] = {
+  args = {
+    "--config-path " .. vim.fn.stdpath "config" .. "/utils/.stylua.toml",
+    "--search-parent-directories",
+    "--stdin-filepath",
+    '"%:p"',
+    "--",
+    "-",
+  },
+  exe = "stylua",
+  stdin = 1,
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 O.user_autocommands = {
-	{ "FileType", "qf", "set nobuflisted" },
-	{ "FileType", "gitcommit", "setlocal nofoldenable spell" },
-	{ "FileType", "git", "setlocal nofoldenable" },
-	{ "FileType", "dashboard", "nnoremap <silent> <buffer> q :q<CR>" },
-	{ "FileType", "lspinfo", "nnoremap <silent> <buffer> q :q<CR>" },
-	{ "FileType", "floaterm", "nnoremap <silent> <buffer> q :q<CR>" },
+  { "FileType", "qf", "set nobuflisted" },
+  { "FileType", "gitcommit", "setlocal nofoldenable spell" },
+  { "FileType", "git", "setlocal nofoldenable" },
+  { "FileType", "dashboard", "nnoremap <silent> <buffer> q :q<CR>" },
+  { "FileType", "lspinfo", "nnoremap <silent> <buffer> q :q<CR>" },
+  { "FileType", "floaterm", "nnoremap <silent> <buffer> q :q<CR>" },
 }
 
 -- Additional Plugins
 O.custom_plugins = {
-	{
-		"wellle/tmux-complete.vim",
-		event = "BufRead",
-		requires = "hrsh7th/nvim-compe",
-	},
-	{ "nanotech/jellybeans.vim" },
-	{
-		"tafryn/vim-tmux-navigator",
-		branch = "forward-script",
-		config = function()
-			vim.g.tmux_navigator_forward_script = "nested_navigate.sh"
-		end,
-	},
-	{
-		"ggandor/lightspeed.nvim",
-		event = "BufRead",
-		config = function()
-			require("lv-lightspeed").config()
-		end,
-	},
-	{
-		"mbbill/undotree",
-		cmd = { "UndotreeFocus", "UndotreeShow", "UndotreeToggle" },
-	},
+  {
+    "wellle/tmux-complete.vim",
+    event = "BufRead",
+    requires = "hrsh7th/nvim-compe",
+  },
+  { "nanotech/jellybeans.vim" },
+  {
+    "tafryn/vim-tmux-navigator",
+    branch = "forward-script",
+    config = function()
+      vim.g.tmux_navigator_forward_script = "nested_navigate.sh"
+    end,
+  },
+  {
+    "ggandor/lightspeed.nvim",
+    event = "BufRead",
+    config = function()
+      require("lv-lightspeed").config()
+    end,
+  },
+  {
+    "mbbill/undotree",
+    cmd = { "UndotreeFocus", "UndotreeShow", "UndotreeToggle" },
+  },
 
-	-- Tpope-ify
-	{ "tpope/vim-repeat", event = "BufRead" },
-	{
-		"tpope/vim-surround",
-		event = "BufRead",
-		config = function()
-			vim.api.nvim_del_keymap("n", "ds")
-			vim.api.nvim_set_keymap("n", "js", "<Plug>Dsurround", {})
-		end,
-	},
-	{ "tpope/vim-unimpaired", event = "BufRead" },
-	{ "tpope/vim-rsi" },
-	{ "tpope/vim-projectionist", event = "BufRead" },
+  -- Tpope-ify
+  { "tpope/vim-repeat", event = "BufRead" },
+  {
+    "tpope/vim-surround",
+    event = "BufRead",
+    config = function()
+      vim.api.nvim_del_keymap("n", "ds")
+      vim.api.nvim_set_keymap("n", "js", "<Plug>Dsurround", {})
+    end,
+  },
+  { "tpope/vim-unimpaired", event = "BufRead" },
+  { "tpope/vim-rsi" },
+  { "tpope/vim-projectionist", event = "BufRead" },
 
-	-- Additional targets
-	{ "wellle/targets.vim", event = "BufRead" },
-	{ "michaeljsmith/vim-indent-object", event = "BufRead" },
-	{ "chaoren/vim-wordmotion", event = "BufRead" },
+  -- Additional targets
+  { "wellle/targets.vim", event = "BufRead" },
+  { "michaeljsmith/vim-indent-object", event = "BufRead" },
+  { "chaoren/vim-wordmotion", event = "BufRead" },
 
-	{ "tommcdo/vim-exchange", event = "BufRead" },
-	{ "tommcdo/vim-lion", event = "BufRead" },
-	{
-		"dyng/ctrlsf.vim",
-		setup = function()
-			vim.g.ctrlsf_default_root = "project"
-			vim.g.ctrlsf_mapping = {
-				open = { "<CR>", "o" },
-				openb = "O",
-				split = "<C-X>",
-				vsplit = "<C-V>",
-				tab = "",
-				tabb = "T",
-				popen = "p",
-				popenf = "P",
-				quit = "q",
-				next = "<C-H>",
-				prev = "<C-T>",
-				pquit = "q",
-				loclist = "",
-				chgmode = "M",
-				stop = "<C-C>",
-			}
-		end,
-	},
-	{
-		"voldikss/vim-floaterm",
-		config = function()
-			vim.g.floaterm_autoinsert = 1
-			vim.g.floaterm_autoclose = 1
-			vim.g.floaterm_title = 0
-			vim.g.floaterm_width = 0.8
-			vim.g.floaterm_height = 0.8
-		end,
-	},
-	{
-		"cdelledonne/vim-cmake",
-		event = "BufRead",
-		config = function()
-			vim.g.cmake_command = "cmake3"
-			vim.g.cmake_jump_on_error = 0
-		end,
-	},
+  { "tommcdo/vim-exchange", event = "BufRead" },
+  { "tommcdo/vim-lion", event = "BufRead" },
+  {
+    "dyng/ctrlsf.vim",
+    setup = function()
+      vim.g.ctrlsf_default_root = "project"
+      vim.g.ctrlsf_mapping = {
+        open = { "<CR>", "o" },
+        openb = "O",
+        split = "<C-X>",
+        vsplit = "<C-V>",
+        tab = "",
+        tabb = "T",
+        popen = "p",
+        popenf = "P",
+        quit = "q",
+        next = "<C-H>",
+        prev = "<C-T>",
+        pquit = "q",
+        loclist = "",
+        chgmode = "M",
+        stop = "<C-C>",
+      }
+    end,
+  },
+  {
+    "voldikss/vim-floaterm",
+    config = function()
+      vim.g.floaterm_autoinsert = 1
+      vim.g.floaterm_autoclose = 1
+      vim.g.floaterm_title = 0
+      vim.g.floaterm_width = 0.8
+      vim.g.floaterm_height = 0.8
+    end,
+  },
+  {
+    "cdelledonne/vim-cmake",
+    event = "BufRead",
+    config = function()
+      vim.g.cmake_command = "cmake3"
+      vim.g.cmake_jump_on_error = 0
+    end,
+  },
 }
