69155b15af52203e53023ca1d96b444e72d49f02
tafryn <tafryn@gmail.com>
2022-02-23 13:22:12 -0800

    Incorporate tmux-navigate plugin for smoother vim/tmux navigation

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -146,6 +146,34 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.cmd("set path+=./include,include;")
 
+-- tmux-navigate support
+vim.cmd([[
+let progname = substitute($VIM, '.*[/\\]', '', '')
+set title titlestring=%{progname}\ %f\ #%{TmuxNavigateDirections()}
+
+function! TmuxNavigateDirections() abort
+  let [y, x] = win_screenpos('.')
+  let h = winheight('.')
+  let w = winwidth('.')
+
+  let can_go_up    = y > 2 " +1 for the tabline
+  let can_go_down  = y + h < &lines - &laststatus
+  let can_go_left  = x > 1
+  let can_go_right = x + w < &columns
+
+  return
+        \ (can_go_up    ? 'U' : '') .
+        \ (can_go_down  ? 'D' : '') .
+        \ (can_go_left  ? 'L' : '') .
+        \ (can_go_right ? 'R' : '')
+endfunction
+
+" enable support for setting the window title in regular Vim under tmux
+if &term =~ '^screen' && !has('nvim')
+  execute "set t_ts=\e]2; t_fs=\7"
+endif
+]])
+
 -- TODO setting foldmethod to "expr" disables treesitter and lsp by default
 -- opt.foldmethod = "expr
 -- generic LSP settings
